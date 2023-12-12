b6b8deba1ba9c19f78fe2cde58e9b77a996bae3f
tanvi4248 <tanvi@GWTN141-10>
2023-12-12 17:15:18 -0600

    Update code

--- src/components/ContactRow.jsx
+++ src/components/ContactRow.jsx
@@ -1,6 +1,6 @@
-function ContactRow({contact}){
+function ContactRow({ setSelectedContactId, contact }){
     return (
-        <tr>
+        <tr onClick={()=>{setSelectedContactId(contact.id)}}>
           <td>{contact.name}</td>
           <td>{contact.email}</td>
           <td>{contact.phone}</td>

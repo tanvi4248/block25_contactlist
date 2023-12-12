55aef2c94b23e88b5f230b13c8b4d31a4f794138
tanvi4248 <tanvi@GWTN141-10>
2023-12-12 17:13:19 -0600

    Change return

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import ContactList from './components/ContactList'
+import SelectedContact from './components/SelectedContact'
+import ContactList from "./components/ContactList";
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+  const[selectedContactId,setSelectedContactId] = useState(null);
 
   return (
     <>
-    <ContactList></ContactList>
-    </>
-  )
+    {selectedContactId ? (
+      <SelectedContact selectedContactId={selectedContactId} setSelectedContactId={setSelectedContactId}/>
+    ) : (
+      <ContactList setSelectedContactId={setSelectedContactId}/>
+    )}
+  </>
+  );
 }
 
 export default App

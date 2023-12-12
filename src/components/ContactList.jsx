24af8ca854d816b6786bc323a1a16937d2626803
tanvi4248 <tanvi@GWTN141-10>
2023-12-12 17:14:50 -0600

    Update code

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,17 +1,25 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ContactRow from "./ContactRow";
 const dummyContacts = [
     { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
     { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
     { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
   ];
-function ContactList(){
+function ContactList({ setSelectedContactId }){
     const [contacts, setContacts] = useState(dummyContacts)
-    console.log("Contacts: ", contacts)
-
-
-
+    useEffect(()=>{
+        async function fetchContacts(){
+          try{
+            const response = await fetch(`https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users`)
+            const data = await response.json()
+            setContacts(data);
+        }catch(error){
+            console.log(error)
+        }
+      }
+        fetchContacts()
+    },[])
     return(
         <table>
           <thead>
@@ -26,8 +34,8 @@ function ContactList(){
               <td>Phone</td>
             </tr>
             {contacts.map((contact) => {
-          return <ContactRow key={contact.id} contact={contact} />;
-        })}
+              return <ContactRow key={contact.id} contact={contact} setSelectedContactId={setSelectedContactId}/>;
+             })}
           </tbody>
         </table>
     )
